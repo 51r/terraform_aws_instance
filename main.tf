70e95cbb94d676665085990964e643896a2eca96
51r <petar.spasov@hashicorp.com>
2022-05-30 09:10:16 +0300

    modified main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,10 @@
-provider "aws" {
-  region = "eu-central-1"
-}
-
 resource "aws_instance" "ubuntu" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
 
   tags = {
     Name = "Peter-Test-Ubuntu"
+    Owner = "petar.spasov@hashicorp.com"
   }
 
 }
